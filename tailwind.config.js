bf998d4d78b7b1bafa0c6762bf698c78ba5effca
Mattia Ferrari <mattia2002.ferrari@gmail.com>
2021-03-19 09:05:53 +0100

    aiut

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
-    purge: [],
+    purge: ['./pages/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',],
     theme: {
         colors: {
             // Build your palette here
