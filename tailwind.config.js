344bf89bd0eaeb76dceb4e2033986be4ed5bb75f
Mattia Ferrari <mattia2002.ferrari@gmail.com>
2021-03-19 09:40:05 +0100

    ok done things

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
     purge: ['./pages/**/*.{js,jsx,ts,tsx}',
         './components/**/*.{js,jsx,ts,tsx}',],
+    darkMode: false,
     theme: {
         colors: {
             // Build your palette here
