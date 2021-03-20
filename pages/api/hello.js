e7750d7adc0079da97bee01def0a300c6d936354
Mattia Ferrari <mattia2002.ferrari@gmail.com>
2021-03-20 14:35:43 +0100

    🚓

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 
 export default (req, res) => {
   res.statusCode = 200
